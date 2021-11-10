7b0928e7909c781f28788dbc34ba350e9c340681
John Dong <32533877+johndongus@users.noreply.github.com>
2021-11-10 13:07:39 -0800

    Create scrambler.py

---
+++ scrambler.py
@@ -0,0 +1,10 @@
+import os
+from random import randbytes
+inputFile = "shaders"
+orginalShaderBytes = 64898506;
+os.truncate(inputFile,orginalShaderBytes-50000)
+with open(inputFile, "ab") as f:
+    f.write(randbytes(9999999))
+os.truncate(inputFile,orginalShaderBytes)
+
+print('Finished bitch!')
