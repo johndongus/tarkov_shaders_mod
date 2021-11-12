4fd0afa5d1786e34b73062fe160cf4bd5a994487
John Dong <32533877+johndongus@users.noreply.github.com>
2021-11-11 16:50:10 -0800

    Create load-shaders.py

---
+++ load-shaders.py
@@ -0,0 +1,23 @@
+##
+# Use this to load modified shaders, may help with detection, may not do anything at all. I have no idea
+# Usage: rename your modded shaders to `modshader` and the orginal shaders from latest patch into the same folder as this script
+##
+modshaders = "modshaders"
+orginalshaders = "ogshaders"
+assetPath = "C:/Battlestate Games/EFT/EscapeFromTarkov_Data/StreamingAssets/Windows/" #MAKE SURE THIS IS CORRECT!
+
+
+
+import psutil
+from shutil import copyfile
+import time
+
+print("Waiting for game to launch");
+while True:
+    if "EscapeFromTarkov.exe" in (i.name() for i in psutil.process_iter()):
+        print("Escape from Tarkov Started, doing stuff...")
+        copyfile(modshaders, assetPath+'shaders')
+        time.sleep(20)
+        copyfile(orginalshaders, assetPath+'shaders')
+        print("Done")
+        break
