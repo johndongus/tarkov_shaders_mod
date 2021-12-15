c3ec63b343c0a0a478f3464bbd00528c7ead48ff
John Dong <32533877+johndongus@users.noreply.github.com>
2021-12-15 14:02:01 -0800

    Update scrambler.py

--- scrambler.py
+++ scrambler.py
@@ -2,7 +2,7 @@ import os
 from random import randbytes
 inputFile = "shaders"
 orginalShaderBytes = 64898506;
-os.truncate(inputFile,orginalShaderBytes-50000)
+os.truncate(inputFile,orginalShaderBytes)
 with open(inputFile, "ab") as f:
     f.write(randbytes(9999999))
 os.truncate(inputFile,orginalShaderBytes)
